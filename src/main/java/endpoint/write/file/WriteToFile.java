323b88f4325b6d60ed1a03ca3ca0c82a118c55fe
Qaiser <qaiser@Qaisers-MacBook-Pro-2.local>
2014-07-21 14:28:41 +0100

    removed endpoint directory

--- src/main/java/endpoint/write/file/WriteToFile.java
+++
@@ -1,9 +0,0 @@
-package endpoint.write.file;
-
-import sad.endpoints.provider.EndPointsProvider;
-
-public interface WriteToFile extends EndPointsProvider {
-	public void setSourceFile(String sourceFileName);
-	public void createHeaderInCSVFile(String fileName);
-	public void writeToCSVFile(String fileName,String endPoint, String status, String timeStamp);
-}
